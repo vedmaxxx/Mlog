cf1902d5407a9284ba492b4452b95ac4d76df3fa
vedmaxxx <jidtirar@gmail.com>
2023-09-24 20:14:03 +0500

    add postStore and commentStore, corrected db.json

---
+++ src/store/commentStore.js
@@ -0,0 +1,34 @@
+import { makeAutoObservable } from "mobx";
+
+class commentStore {
+  comments = [
+    {
+      postId: 1,
+      id: 1,
+      userId: 1,
+      body: "harum non quasi et ration tempore iure ex voluptates in rationeharum architecto fugit inventore cupiditatevoluptates magni quo et",
+    },
+    {
+      postId: 1,
+      id: 2,
+      userId: 2,
+      body: "harum non quasi et ration tempore iure ex voluptates in rationeharum architecto fugit inventore cupiditatevoluptates magni quo et",
+    },
+  ];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  // Добавление/удаление комментариев
+  addComment(comment) {
+    this.comments.push(comment);
+  }
+  removeComment(comment_id) {
+    this.comments = this.comments.filter(
+      (comment) => comment.id !== comment_id
+    );
+  }
+}
+
+export default new commentStore();
---
+++ src/store/postStore.js
@@ -0,0 +1,40 @@
+import { makeAutoObservable } from "mobx";
+
+class postStore {
+  posts = [
+    {
+      id: 1,
+      userId: 1,
+      title: "Which programming language is the best?",
+      body: "some text",
+      watches: 30,
+      likes: 10,
+      date: "2023-09-10",
+      image: "",
+    },
+    {
+      id: 2,
+      userId: 2,
+      title: "C++ basics",
+      body: "some text",
+      watches: 14,
+      likes: 4,
+      date: "2023-09-11",
+      image: "",
+    },
+  ];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  // Добавление/удаление постов
+  addPost(post) {
+    this.posts.push(post);
+  }
+  removePost(post_id) {
+    this.posts = this.posts.filter((post) => post.id !== post_id);
+  }
+}
+
+export default new postStore();
