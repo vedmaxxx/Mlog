ea18f32375c2e98e7f84cd4c093b4582a39c10de
vedmaxxx <jidtirar@gmail.com>
2023-10-06 17:34:20 +0500

    working on editPostMenu

--- src/components/EditPostMenu.jsx
+++ src/components/EditPostMenu.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import classes from "./EditPostMenu.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const EditPostMenu = () => {
   return (
-    <Container>
-      <div>
-        <div>Название поста</div>
+    <Container className={classes.container}>
+      <div className={classes.title}>Новый пост</div>
+      <div className={classes.grid}>
+        <div>Заголовок поста</div>
         <div>
-          <input type="text" />
+          <input
+            className={classes.input}
+            type="text"
+            placeholder="Введите заголовок поста..."
+          />
         </div>
         <div>Анонс</div>
         <div>
-          <input type="text" />
+          <input
+            className={classes.input}
+            type="text"
+            placeholder="Введите небольшое описание текста..."
+          />
         </div>
       </div>
     </Container>
