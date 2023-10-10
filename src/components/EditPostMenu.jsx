d470f0ef2ee122f6d5dc44c208dee9fac2ca5c09
vedmaxxx <jidtirar@gmail.com>
2023-10-10 18:59:48 +0500

    add first version of editmenu

--- src/components/EditPostMenu.jsx
+++ src/components/EditPostMenu.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import classes from "./EditPostMenu.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Editor } from "@tinymce/tinymce-react";
 
 const EditPostMenu = () => {
+  const handleEditorChange = (e) => {
+    console.log(e.target.getContent());
+  };
+
   return (
     <Container className={classes.container}>
       <div className={classes.title}>Новый пост</div>
@@ -24,6 +29,28 @@ const EditPostMenu = () => {
             placeholder="Введите небольшое описание текста..."
           />
         </div>
+        <div>Обложка</div>
+        <div>
+          <input
+            className={classes.file_input}
+            type="file"
+            placeholder="Введите небольшое описание текста..."
+          />
+        </div>
+        <div></div>
+        <div>
+          <Editor
+            content="<p>This is the initial content of the editor</p>"
+            config={{
+              plugins: "autolink link image lists print preview",
+              toolbar:
+                "undo redo | bold italic | alignleft aligncenter alignright",
+            }}
+            onChange={console.log("заработало")}
+          />
+        </div>
+        <div></div>
+        <button className={classes.post_btn}>Опубликовать</button>
       </div>
     </Container>
   );
